63007e4122c91c0b4767640404c93d0443932b05
bentemplin <bentemplin@users.noreply.github.com>
2019-11-21 16:53:47 -0500

    Add CFG generator and In-Set and Out-Set calculation for blocks and instructions.

--- TigerBackend.java
+++ TigerBackend.java
@@ -7,16 +7,22 @@ public class TigerBackend {
     public static void main(String[] args) throws IOException {
         String sourceFile = args[0];
 
-        BlockGenerator generator = new BlockGenerator(sourceFile);
+        CFGGenerator generator = new CFGGenerator(sourceFile);
         
         generator.generateBlocks();
 
-        System.out.println("ENTRY:");
-        System.out.println(generator.getEntryBlock());
+        // System.out.println("ENTRY:");
+        // System.out.println(generator.getEntryBlock());
 
-        System.out.println("\n\nBLOCKS");
-        for (BasicBlock block : generator.getBlocks()) {
-            System.out.println(block + "\n");
+        // System.out.println("\n\nBLOCKS");
+        // for (BasicBlock block : generator.getBlocks()) {
+        //     System.out.println(block + "\n");
+        // }
+
+        generator.generateInOutSets();
+
+        for (BasicBlock b : generator.getBlocks()) {
+            System.out.println(String.format("BLOCK: %s | IN: %s | OUT: %s", b.getBlockName(), b.inSet, b.outSet));
         }
     }
 }
