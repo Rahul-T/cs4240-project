dab8f66ebfd181b434e3fb2befe1932b5c3eece7
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2019-11-20 23:05:43 -0500

    Added setup stage with global variable tracking

--- NaiveAllocator.java
+++ NaiveAllocator.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 
 public class NaiveAllocator {
     private String irFile;
@@ -10,10 +10,170 @@ public class NaiveAllocator {
         this.irFile = irFile;
     }
 
+    public void setup() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(this.irFile));
+        String line;
+        HashMap<String, String> globalVars = new HashMap<String, String>();
+        HashMap<String, HashMap<String, String>> functionToVars = new HashMap<String, HashMap<String, String>>();
+        String currentFunction = "";
+        boolean isMain = false;
+
+        while ((line = br.readLine()) != null) {
+            // Comment
+            if(line.contains("#")) {
+                continue;
+            }
+            // Function header
+            else if(line.contains("(") && line.contains(")")) {
+                isMain = line.contains("void main()");
+
+                HashMap<String, String> declaredVars = new HashMap<String, String>();
+                currentFunction = line.substring(line.indexOf(" ")+1, line.indexOf("("));
+                functionToVars.put(currentFunction, declaredVars);
+
+                String[] paramsList = line.substring(line.indexOf("(")+1, line.indexOf(")")).split(",");
+                for(String param: paramsList) {
+                    if(param.length() == 0) {
+                        break;
+                    }
+                    String[] typeAndName = param.trim().split(" ");
+                    declaredVars.put(typeAndName[1], typeAndName[0]);
+                }
+
+                // int-list 
+                line = br.readLine();
+                String[] intList = line.substring(line.indexOf(":")+1).split(",");
+                for(String param: intList) {
+                    param = param.trim();
+                    if(param.length() == 0) {
+                        break;
+                    }
+                    if(param.contains("[")) {
+                        globalVars.put(param, "intarray");
+                        declaredVars.put(param, "intarray");
+                    } else {
+                        declaredVars.put(param, "int");
+                    }
+                }
+
+                // float-list 
+                line = br.readLine();
+                String[] floatList = line.substring(line.indexOf(":")+1).split(",");
+                for(String param: floatList) {
+                    param = param.trim();
+                    if(param.length() == 0) {
+                        break;
+                    }
+                    if(param.contains("[")) {
+                        globalVars.put(param, "floatarray");
+                        declaredVars.put(param, "floatarray");
+                    } else {
+                        declaredVars.put(param, "float");
+                    }
+                }
+            }
+            // Regular statements
+            else if (!line.contains(":") && !isMain) {
+                String[] lineElements = line.trim().split(",");
+                
+                // System.out.println(Arrays.toString(lineElements));
+                switch (lineElements[0]) {
+                    case "assign":
+                    case "add":
+                    case "sub":
+                    case "mult":
+                    case "div":
+                    case "and":
+                    case "or":
+                    case "return":
+                    case "array_store":
+                    case "array_load":
+                        for(int i=1; i<lineElements.length; i++) {
+                            if(isNumeric(lineElements[i])) {
+                                continue;
+                            }
+                            if(!functionToVars.get(currentFunction).keySet().contains(lineElements[i])) {
+                                System.out.println(lineElements[i]);
+                                globalVars.put(lineElements[i], "");
+                            }
+                        }
+                        break;
+                    case "breq":
+                    case "brneq":
+                    case "brlt":
+                    case "brgt":
+                    case "brgeq":
+                    case "brleq":
+                        for(int i=1; i<lineElements.length-1; i++) {
+                            if(isNumeric(lineElements[i])) {
+                                continue;
+                            }
+                            if(!functionToVars.get(currentFunction).keySet().contains(lineElements[i])) {
+                                System.out.println(lineElements[i]);
+                                globalVars.put(lineElements[i], "");
+                            }
+                        }
+                        break;
+                    case "call":
+                        for(int i=2; i<lineElements.length; i++) {
+                            if(isNumeric(lineElements[i])) {
+                                continue;
+                            }
+                            if(!functionToVars.get(currentFunction).keySet().contains(lineElements[i])) {
+                                System.out.println(lineElements[i]);
+                                globalVars.put(lineElements[i], "");
+                            }
+                        }
+                        break;
+                    case "callr":
+                        if(isNumeric(lineElements[1])) {
+                            continue;
+                        }
+                        if(!functionToVars.get(currentFunction).keySet().contains(lineElements[1])) {
+                            System.out.println(lineElements[1]);
+                            globalVars.put(lineElements[1], "");
+                        }
+                        for(int i=3; i<lineElements.length; i++) {
+                            if(isNumeric(lineElements[i])) {
+                                continue;
+                            }
+                            if(!functionToVars.get(currentFunction).keySet().contains(lineElements[i])) {
+                                System.out.println(lineElements[i]);
+                                globalVars.put(lineElements[i], "");
+                            }
+                        }
+                        break;
+                        
+                }
+            }
+        }
+        for (String globalVar: globalVars.keySet()) {
+            globalVars.put(globalVar, functionToVars.get("main").get(globalVar));
+        }
+
+        System.out.println("Function To Vars");
+        functionToVars.entrySet().forEach(entry->{
+            System.out.println(entry.getKey() + " " + entry.getValue());  
+        });
+        System.out.println("Global Vars");
+        globalVars.entrySet().forEach(entry->{
+            System.out.println(entry.getKey() + " " + entry.getValue());  
+        });
+
+    }
+
     public ArrayList<String> generatemips() throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(this.irFile));
         String line;
         ArrayList<String> mips = new ArrayList<>();
+
+        // mips.add(".data");
+        // String intDecl = br.readLine();
+        // mips.add(intDecl);
+        // String floatDecl = br.readLine();
+        // mips.add(floatDecl);
+        // mips.add(".text");
+
         while ((line = br.readLine()) != null) {
             // System.out.println(line);
             String[] lineElements = line.replaceAll(",","").trim().split(" ");
@@ -102,6 +262,7 @@ public class NaiveAllocator {
                         break;
 
                     default: {
+                        mips.add(line);
                         continue;
                     }
                 }
@@ -131,7 +292,8 @@ public class NaiveAllocator {
     }
 
     public static void main(String[] args) throws IOException{
-        NaiveAllocator naiveAllocator = new NaiveAllocator("Testing/example.ir");
-        ArrayList<String> ir = naiveAllocator.generatemips();
+        NaiveAllocator naiveAllocator = new NaiveAllocator("Testing/test1.ir");
+        // ArrayList<String> ir = naiveAllocator.generatemips();
+        naiveAllocator.setup();
     }
 }
