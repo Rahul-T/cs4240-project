f617dec27b4a8bc9339413710c3ba2d21ca456ce
bentemplin <bentemplin@users.noreply.github.com>
2019-12-02 14:32:07 -0500

    Update project_test.sh
    Now you only need SPIMPATH if spim isn't installed as a command on your computer

--- project_test.sh
+++ project_test.sh
@@ -12,37 +12,37 @@ java TigerBackend spill.ir >> ../junk.txt
 java TigerBackend test1.ir >> ../junk.txt
 
 echo cascade
-$SPIMPATH -keepstats -f P2Output/cascade_naive.s 
-$SPIMPATH -keepstats -f P2Output/cascade_colored.s 
+${SPIMPATH}spim -keepstats -f P2Output/cascade_naive.s 
+${SPIMPATH}spim -keepstats -f P2Output/cascade_colored.s 
 
 echo factorial
-$SPIMPATH -keepstats -f P2Output/factorial_naive.s 
-$SPIMPATH -keepstats -f P2Output/factorial_colored.s 
+${SPIMPATH}spim -keepstats -f P2Output/factorial_naive.s 
+${SPIMPATH}spim -keepstats -f P2Output/factorial_colored.s 
 
 echo float_cascade
-$SPIMPATH -keepstats -f P2Output/float_cascade_naive.s 
-$SPIMPATH -keepstats -f P2Output/float_cascade_colored.s
+${SPIMPATH}spim -keepstats -f P2Output/float_cascade_naive.s 
+${SPIMPATH}spim -keepstats -f P2Output/float_cascade_colored.s
 
 echo float_global_param
-$SPIMPATH -keepstats -f P2Output/float_global_param_naive.s 
-$SPIMPATH -keepstats -f P2Output/float_global_param_colored.s
+${SPIMPATH}spim -keepstats -f P2Output/float_global_param_naive.s 
+${SPIMPATH}spim -keepstats -f P2Output/float_global_param_colored.s
 
 # echo nqueens
-# $SPIMPATH -keepstats -f P2Output/nqueens_naive.s 
-# $SPIMPATH -keepstats -f P2Output/nqueens_colored.s 
+# ${SPIMPATH}spim -keepstats -f P2Output/nqueens_naive.s 
+# ${SPIMPATH}spim -keepstats -f P2Output/nqueens_colored.s 
 
 echo perfect_sqrt
-$SPIMPATH -keepstats -f P2Output/perfect_sqrt_naive.s 
-$SPIMPATH -keepstats -f P2Output/perfect_sqrt_colored.s
+${SPIMPATH}spim -keepstats -f P2Output/perfect_sqrt_naive.s 
+${SPIMPATH}spim -keepstats -f P2Output/perfect_sqrt_colored.s
 
 echo sort
-$SPIMPATH -keepstats -f P2Output/sort_naive.s 
-$SPIMPATH -keepstats -f P2Output/sort_colored.s 
+${SPIMPATH}spim -keepstats -f P2Output/sort_naive.s 
+${SPIMPATH}spim -keepstats -f P2Output/sort_colored.s 
 
 echo spill
-$SPIMPATH -keepstats -f P2Output/spill_naive.s 
-$SPIMPATH -keepstats -f P2Output/spill_colored.s 
+${SPIMPATH}spim -keepstats -f P2Output/spill_naive.s 
+${SPIMPATH}spim -keepstats -f P2Output/spill_colored.s 
 
 echo test1
-$SPIMPATH -keepstats -f P2Output/test1_naive.s 
-$SPIMPATH -keepstats -f P2Output/test1_colored.s 
+${SPIMPATH}spim -keepstats -f P2Output/test1_naive.s 
+${SPIMPATH}spim -keepstats -f P2Output/test1_colored.s 
